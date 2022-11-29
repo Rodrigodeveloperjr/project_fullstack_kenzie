51cf54cc131de1408ea382be9038388ea03135fb
Rodrigodeveloperjr <rodrigojsdeveloper@gmail.com>
2022-11-29 12:22:22 -0300

    error handling done

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,13 @@
+import { handleErrorMiddleware } from './middlewares/handleError.middleware'
 import express from 'express'
+import 'express-async-errors'
+import 'reflect-metadata'
+import 'dotenv'
 
 
 const app = express()
-
 app.use(express.json())
 
+app.use(handleErrorMiddleware)
 
 export { app }
